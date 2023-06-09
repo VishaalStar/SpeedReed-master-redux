627431ee132e39b9932c196c69c30b01e56d879f
VishaalStar <135718716+VishaalStar@users.noreply.github.com>
2023-06-09 12:58:26 +0530

    added redux

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = {
+    experimental: {
+      turbo: {
+        loaders: {
+          // Option format
+          '.md': [
+            {
+              loader: '@mdx-js/loader',
+              options: {
+                format: 'md',
+              },
+            },
+          ],
+          // Option-less format
+          '.mdx': ['@mdx-js/loader'],
+        },
+      },
+    },
+  };
